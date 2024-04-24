fecbcab0dc821386a8b5ce0559ba25edd75cd9c2
John Huang <pheno_the_best@yahoo.com.tw>
2024-04-24 23:52:01 +0800

    safe area

--- App.js
+++ App.js
@@ -1,3 +1,12 @@
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { Text } from "react-native";
+
 export default function App() {
-  return <></>;
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <Text>Price Notifier!</Text>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
