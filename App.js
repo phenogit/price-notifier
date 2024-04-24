a50551ab75c442d9c863aede8c3d72d22961cb79
John Huang <pheno_the_best@yahoo.com.tw>
2024-04-25 01:14:11 +0800

    simple layout

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import { styles } from "./App.style";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView>
-        <Text>Price Notifier!</Text>
+      <SafeAreaView style={styles.app}>
+        <View style={styles.header}>
+          <Text>Kandan+: Header</Text>
+        </View>
+        <View style={styles.bpdy}>
+          <Text>Body</Text>
+        </View>
+        <View style={styles.footer}>
+          <Text>Footer</Text>
+        </View>
       </SafeAreaView>
     </SafeAreaProvider>
   );
--- App.style.js
+++ App.style.js
@@ -1,3 +1,26 @@
 import { StyleSheet } from "react-native";
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  app: {
+    flex: 1,
+    backgroundColor: "#F9F9F9",
+  },
+  header: {
+    flex: 1,
+    backgroundColor: "#FF0000",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  bpdy: {
+    flex: 8,
+    backgroundColor: "#00FF00",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  footer: {
+    flex: 1,
+    backgroundColor: "#0000FF",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
