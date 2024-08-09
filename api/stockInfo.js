7bd7d7e633ddc775a52c568b0b7b4478e7caf0bd
john.huang <pheno_the_best@yahoo.com.tw>
2024-08-09 13:55:36 +0800

    update packages

--- api/stockInfo.js
+++ api/stockInfo.js
@@ -1,3 +1,10 @@
+import axios from "axios";
+
+const FUGLE_API_URL =
+  "https://price-provider-fdfhe0dudscuejhm.eastasia-01.azurewebsites.net/";
+
 export class StockInfoAPI {
-  static async getStockInfo(stockId) {}
+  static async getStockInfo(stockId) {
+    return (await axios.get(`${FUGLGE_API_URL}?stockId=${stockId}`)).data;
+  }
 }
